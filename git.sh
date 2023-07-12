f51575b3a068c9630d9d82b2d490e15fa6eeb329
freather-ben <3440423865@qq.com>
2023-07-12 13:35:27 +0800

    x201i

---
+++ git.sh
@@ -0,0 +1,5 @@
+git clone git@github.com:1821653110Dx/note.git
+git clone git@github.com:1821653110Dx/swap.git
+git clone git@github.com:1821653110Dx/temp.git
+git clone git@github.com:1821653110Dx/work.git
+git config --global --add safe.directory '*'
