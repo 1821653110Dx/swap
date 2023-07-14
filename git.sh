f8b575d94eb110cdc9073353ae97676c27361797
Freather Ben <101937162+1821653110Dx@users.noreply.github.com>
2023-07-14 21:27:42 +0800

    Delete git.sh

--- git.sh
+++
@@ -1,5 +0,0 @@
-git clone git@github.com:1821653110Dx/note.git
-git clone git@github.com:1821653110Dx/swap.git
-git clone git@github.com:1821653110Dx/temp.git
-git clone git@github.com:1821653110Dx/work.git
-git config --global --add safe.directory '*'
