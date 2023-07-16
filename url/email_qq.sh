291b49ee0f3c0c5979d92bd97ac6c6425535f13d
freather-ben <3440423865@qq.com>
2023-07-16 17:31:51 +0800

    asd

--- url/email_qq.sh
+++
@@ -1,1 +0,0 @@
-firefox https://mail.qq.com/cgi-bin/frame_html?sid=X_ZJ7F_F_elO3RB0&r=8d2805430f77192540da09fd6d9b6226&lang=zh
--- url/jwpt_smxy.sh
+++
@@ -1,1 +0,0 @@
-firefox http://jwxxt.fjsmu.edu.cn/jwglxt/xtgl/index_initMenu.html?jsdm=xs&_t=1688721862343
